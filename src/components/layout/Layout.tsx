bec64ea8d6a7e56b0e5e1b7ca4fe8ed78d1aaf63
k3cman <kecman.dev@gmail.com>
2020-06-16 22:52:07 +0200

    setup base of the layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,22 @@
 import React, { FunctionComponent } from "react";
 import { Header } from "./Header";
+import styles from "./Layout.module.scss";
 import { Main } from "./Main";
 import { Sidebar } from "./Sidebar";
 
 export const Layout: FunctionComponent = () => {
   return (
-    <div className="app-wrapper">
-      <div className="side">
+    <div className={styles["app-wrapper"]}>
+      <div className={styles.side}>
         <Sidebar />
       </div>
-      <div className="content">
-        <Header />
-        <Main />
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <Header />
+        </div>
+        <div className={styles.main}>
+          <Main />
+        </div>
       </div>
     </div>
   );
